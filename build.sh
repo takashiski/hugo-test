9f57a6bab531f8f31b9a376a08cb0f67a44044f0
takashiski <t_watanabe@denkiyagi.jp>
2022-01-31 17:55:03 +0900

    -Uオプションを追加

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 gem --version
 bundle --version
-gem environment
-gem install bundler
-bundle --version
-gem environment
-gem install asciidoctor -v 2.0
-ls /opt/buildhome/.rvm/gems/ruby-2.7.1/bin/
-ls /opt/buildhome/.rvm/gems/ruby-2.7.1/
+# gem environment
+# gem install bundler
+# bundle --version
+# gem environment
+gem install -U asciidoctor -v 2.0
+# ls /opt/buildhome/.rvm/gems/ruby-2.7.1/bin/
+# ls /opt/buildhome/.rvm/gems/ruby-2.7.1/
 hugo
