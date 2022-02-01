6b6890eae5c6ae64d022dadc40dd1e9469267acf
takashiski <t_watanabe@denkiyagi.jp>
2022-02-01 11:36:19 +0900

    distribution checkを追加

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-gem --version
-bundle --version
+# gem --version
+# bundle --version
 # gem environment
 # gem install bundler
 # bundle --version
 # gem environment
-gem update --system
-gem install asciidoctor -v 2.0
+# gem update --system
+ls /etc/*-release
+# gem install asciidoctor
 # ls /opt/buildhome/.rvm/gems/ruby-2.7.1/bin/
 # ls /opt/buildhome/.rvm/gems/ruby-2.7.1/
 hugo
