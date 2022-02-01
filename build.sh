b4a7de5543dab64473cc2d07505bb5ca83b0f314
takashiski <t_watanabe@denkiyagi.jp>
2022-02-01 12:07:00 +0900

    asciidoctorがどこにインストールされているか調べる

--- build.sh
+++ build.sh
@@ -10,7 +10,11 @@ gem environment
 # cat /etc/os-release
 gem install asciidoctor
 echo $PATH
-apt-get install asciidoctor
+sudo apt-get install asciidoctor
+echo "Show in /opt/buildhome/.rvm/gems/ruby-2.7.1"
+ls /opt/buildhome/.rvm/gems/ruby-2.7.1
+echo "Show in /opt/buildhome/.gem/ruby/2.7.0"
+ls /opt/buildhome/.gem/ruby/2.7.0
 # gem install asciidoctor
 # ls /opt/buildhome/.rvm/gems/ruby-2.7.1/bin/
 # ls /opt/buildhome/.rvm/gems/ruby-2.7.1/
