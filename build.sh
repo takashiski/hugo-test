cb8a7b37c6be6d877050588a778a8fba232ab72d
takashiski <t_watanabe@denkiyagi.jp>
2022-02-01 15:35:53 +0900

    build.shを整理

--- build.sh
+++ build.sh
@@ -1,24 +1,2 @@
-# gem --version
-# bundle --version
-# gem environment
-# gem install bundler
-# bundle --version
-gem environment
-# gem update --system
-# ls /etc/*-release
-# cat /etc/lsb-release
-# cat /etc/os-release
-# gem install asciidoctor
-# asciidoctor --version
-# gem install asciidoctor --no-user-install
 npm install -g asciidoctor
-# echo $PATH
-# sudo apt-get install asciidoctor
-# echo "Show in /opt/buildhome/.rvm/gems/ruby-2.7.1/bin"
-# ls /opt/buildhome/.rvm/gems/ruby-2.7.1/bin
-# echo "Show in /opt/buildhome/.gem/ruby/2.7.0"
-# ls /opt/buildhome/.gem/ruby/2.7.0
-# gem install asciidoctor
-# ls /opt/buildhome/.rvm/gems/ruby-2.7.1/bin/
-# ls /opt/buildhome/.rvm/gems/ruby-2.7.1/
 hugo
