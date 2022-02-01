5093e05274e8276cc6f2fbd6d2d1c06f9202a77e
takashiski <t_watanabe@denkiyagi.jp>
2022-02-01 11:58:54 +0900

    gem enviroment追加

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 # gem environment
 # gem install bundler
 # bundle --version
-# gem environment
+gem environment
 # gem update --system
 # ls /etc/*-release
 # cat /etc/lsb-release
 # cat /etc/os-release
 gem install asciidoctor
 echo $PATH
-apt install asciidoctor
+apt-get install asciidoctor
 # gem install asciidoctor
 # ls /opt/buildhome/.rvm/gems/ruby-2.7.1/bin/
 # ls /opt/buildhome/.rvm/gems/ruby-2.7.1/
